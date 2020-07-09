b145e328119a2849d4f4c07cafc9bc52e912e4f4
HyeonWoo Park <sunghakzz@naver.com>
2020-07-09 16:15:39 +0900

    11283.한글 2
    Signed-off-by: HyeonWoo Park <sunghakzz@naver.com>

---
+++ hyeonwoo/11283.한글 2.py
@@ -0,0 +1,26 @@
+"""
+    File: 11283.py
+    Title: 한글 2
+    URL: https://www.acmicpc.net/problem/11283
+    Input #1:
+        가
+    Output #1:
+        1
+    Input #2:
+        힣
+    Output #2:
+        11172
+    Input #3:
+        백
+    Output #3:
+        4146
+    Input #4:
+        준
+    Output #4:
+        7425
+    Created At: 2020-07-09 16:12:39.876878
+"""
+
+print(
+    ord(input()) - ord('가') + 1
+)
